73a304bd24d6655892bfcd184b8b3e615f9619d6
aman <aman.jangra@celebal.tech>
2022-12-01 15:52:11 +0530

    fix(user): fix

--- code.bash
+++ code.bash
@@ -1,20 +1,27 @@
-mystr="$(grep "version" package.json)"
+# mystr="$(grep "version" package.json)"
 
-ans=""
+# echo $mystr
 
-echo $mystr
+# ans=""
 
-readarray -d "." -t strarr<<<"$mystr"
-for((n=0; n<${#strarr[*]}-1; n++));
-do
-	ans+="${strarr[n]}.";
-done
+# readarray -d "." -t strarr<<<"$mystr"
+# for((n=0; n<${#strarr[*]}-1; n++));
+# do
+# 	ans+="${strarr[n]}.";
+# done
 
+# curr=${strarr[-1]}
 
-file_name="package.json"
-
-ans+="${nextRelease}\","
-
-echo $ans
+# curr=${curr:(0):-3}
 
 
+# updated_version=$(($curr+1))
+
+
+# file_name="package.json"
+
+# ans+="${updated_version}\","
+
+# sed -i "s/$mystr/$ans/" $file_name
+
+echo $nextRelease
