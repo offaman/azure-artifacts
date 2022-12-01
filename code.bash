0e50db8bafa77ea359e8860e67cddf8ce4936b05
aman <aman.jangra@celebal.tech>
2022-12-01 19:16:51 +0530

    fix(fix): fix

--- code.bash
+++ code.bash
@@ -14,7 +14,6 @@
 
 # curr=${curr:(0):-3}
 
-
 # updated_version=$(($curr+1))
 
 
@@ -32,9 +31,6 @@
 
 # final=""
 
-
-
-
 # strarr=$(echo $mystr | tr ":" "\n") 
 
 # new_version=""
@@ -44,35 +40,20 @@
 # #     echo $addr
 # # done
 
-
-
 # for (( n=0; n < ${#strarr[*]}; n++ ))  
 # do  
 # echo "${strarr[n]}"  
 # done
 
-
 # # strarr[1]="\"$nextRelease\"," && v1=${strarr[0]} && v2=${strarr[1]} && spc=": "
 
 # # new_version=$v1$spc$v2 &&
-
-
-
 # # curr=$(echo $mystr | tr ":" " ")
-
 
 # # final+=${mails[0]}
 # # final+=${mails[1]}
 # # echo $final
 
-
-
-
-
-
-
-
 # p1="\"version"\" && p2=": " && p3="\"$nextRelease\"," && final_version=$p1$p2$p3 && file_name="package.json" && mystr="$(grep "version" package.json)" &&sed -i "s/$mystr/$final_version/" $file_name 
 
-
-echo $New_version
+# echo $New_version
