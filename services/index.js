372b2509591654d742abdcd4c1f6ad8368fcc128
aman <aman.jangra@celebal.tech>
2022-12-01 18:31:19 +0530

    fix: fix bugs

--- services/index.js
+++ services/index.js
@@ -3,17 +3,4 @@ console.log("Azure artifacts")
 console.log("bug fixed")
 
 
-console.log("new feature released")
-
-console.log("changing npm version")
-
-console.log("Azure artifacts")
-
-console.log("bug fixed")
-
-
-console.log("new feature released")
-
-console.log("changing npm version")
-
 
