8fb0454f1e378c2f173fdf52da216d087dfa5f90
aman <aman.jangra@celebal.tech>
2022-12-01 18:06:41 +0530

    feat(user): feat added

--- services/index.js
+++ services/index.js
@@ -7,3 +7,13 @@ console.log("new feature released")
 
 console.log("changing npm version")
 
+console.log("Azure artifacts")
+
+console.log("bug fixed")
+
+
+console.log("new feature released")
+
+console.log("changing npm version")
+
+
